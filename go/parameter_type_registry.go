ttomasini/cucumber

cucumber-expressions: Add anonymous parameter types
Introduces the anonymous parameter to cucumber expressions. This is
a breaking change because we're installing a new expression in the
parameter type registry.

\#\# Details ##

Installs the anonymous parameter type into the registry:
```java
ParameterType(
  name: ""
  regexps: [\.*\]
  transformer: s -> s
  useForSnippets: false
  preferForRegexMatch: true
)
```
For Javascript and Ruby this will merely returns the captured text as
a string.

However for strongly typed languages like Java and Go this make this
allows the step definition to hint the type to which the captured text
should be converted. When used in conjunction with an object mapper
this makes it easier to quickly define step definitions without the need
to define parameter types for each type. For example in Java the following
will work without the need to configure additional parameter types.

```java
Given("^there is some date (.*)$", (Date a) -> { })

Given("there is some date {}", (Date a) -> { })

Given("there is some transaction {} {}", (BigDecimal a, Currency b) -> { })

Given("there is some type {}", (TypeEnum a) -> { })
```

To make the dynamic transform possible type hints are passed to the cucumber
expression when matching a step. Using these hints the `s -> s` transform is
replaced with `s -> defaultTransform.transform(s, typeHint)`. A simple
implementation of the `defaultTransform` is provided that supports a selection
of primitives but should generally speaking be replaced with a proper object mapper.

Fixes: #493

\#\# Motivation and Context \#\#

To make it easier to upgrade from Cucumber-JVM v3 to v4.
See https://github.com/cucumber/cucumber-jvm/issues/1478 for details.

cucumber-expressions/go/parameter_type_registry.go
@@ -2,9 +2,9 @@ package cucumberexpressions
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 	"sort"
-	"strconv"
 )
 
 var INTEGER_REGEXPS = []*regexp.Regexp{
@@ -20,23 +20,28 @@ var WORD_REGEXPS = []*regexp.Regexp{
 var STRING_REGEXPS = []*regexp.Regexp{
 	regexp.MustCompile(`"([^"\\]*(\\.[^"\\]*)*)"|'([^'\\]*(\\.[^'\\]*)*)'`),
 }
+var ANONYMOUS_REGEXPS = `.*`
 
 type ParameterTypeRegistry struct {
 	parameterTypeByName    map[string]*ParameterType
 	parameterTypesByRegexp map[string][]*ParameterType
+	defaultTransformer     DefaultTransformer
 }
 
 func NewParameterTypeRegistry() *ParameterTypeRegistry {
+	transformer := SimpleDefaultTransformer{}
+
 	result := &ParameterTypeRegistry{
 		parameterTypeByName:    map[string]*ParameterType{},
 		parameterTypesByRegexp: map[string][]*ParameterType{},
+		defaultTransformer:     transformer,
 	}
 	intParameterType, err := NewParameterType(
 		"int",
 		INTEGER_REGEXPS,
 		"int",
 		func(args ...*string) interface{} {
-			i, err := strconv.Atoi(*args[0])
+			i, err := transformer.Transform(*args[0], reflect.Int)
 			if err != nil {
 				panic(err)
 			}
@@ -54,7 +59,7 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		FLOAT_REGEXPS,
 		"float",
 		func(args ...*string) interface{} {
-			f, err := strconv.ParseFloat(*args[0], 64)
+			f, err := transformer.Transform(*args[0], reflect.Float64)
 			if err != nil {
 				panic(err)
 			}
@@ -72,7 +77,11 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		WORD_REGEXPS,
 		"string",
 		func(args ...*string) interface{} {
-			return *args[0]
+			i, err := transformer.Transform(*args[0], reflect.String)
+			if err != nil {
+				panic(err)
+			}
+			return i
 		},
 		false,
 		false,
@@ -87,9 +96,17 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		"string",
 		func(args ...*string) interface{} {
 			if args[0] == nil && args[1] != nil {
-				return *args[1]
+				i, err := transformer.Transform(*args[1], reflect.String)
+				if err != nil {
+					panic(err)
+				}
+				return i
 			}
-			return *args[0]
+			i, err := transformer.Transform(*args[0], reflect.String)
+			if err != nil {
+				panic(err)
+			}
+			return i
 		},
 		true,
 		false,
@@ -98,6 +115,13 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		panic(err)
 	}
 	result.DefineParameterType(stringParameterType)
+
+	anonymouseParameterType, err := createAnonymousParameterType(ANONYMOUS_REGEXPS)
+	if err != nil {
+		panic(err)
+	}
+	result.DefineParameterType(anonymouseParameterType)
+
 	return result
 }
 
@@ -128,12 +152,13 @@ func (p *ParameterTypeRegistry) LookupByRegexp(parameterTypeRegexp string, expre
 }
 
 func (p *ParameterTypeRegistry) DefineParameterType(parameterType *ParameterType) error {
-	if len(parameterType.Name()) > 0 {
-		if _, ok := p.parameterTypeByName[parameterType.Name()]; ok {
-			return fmt.Errorf("There is already a parameter type with name %s", parameterType.Name())
+	if _, ok := p.parameterTypeByName[parameterType.Name()]; ok {
+		if len(parameterType.Name()) == 0 {
+			return fmt.Errorf("The anonymous parameter type has already been defined")
 		}
-		p.parameterTypeByName[parameterType.Name()] = parameterType
+		return fmt.Errorf("There is already a parameter type with name %s", parameterType.Name())
 	}
+	p.parameterTypeByName[parameterType.Name()] = parameterType
 	for _, parameterTypeRegexp := range parameterType.Regexps() {
 		if _, ok := p.parameterTypesByRegexp[parameterTypeRegexp.String()]; !ok {
 			p.parameterTypesByRegexp[parameterTypeRegexp.String()] = []*ParameterType{}

cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/CucumberExpression.java
@@ -1,5 +1,6 @@
 package io.cucumber.cucumberexpressions;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -9,7 +10,7 @@ public class CucumberExpression implements Expression {
     // Does not include (){} characters because they have special meaning
     private static final Pattern ESCAPE_PATTERN = Pattern.compile("([\\\\^\\[$.|?*+\\]])");
     @SuppressWarnings("RegExpRedundantEscape") // Android can't parse unescaped braces
-    static final Pattern PARAMETER_PATTERN = Pattern.compile("(\\\\\\\\)?\\{([^}]+)\\}");
+    static final Pattern PARAMETER_PATTERN = Pattern.compile("(\\\\\\\\)?\\{([^}]*)\\}");
     private static final Pattern OPTIONAL_PATTERN = Pattern.compile("(\\\\\\\\)?\\(([^)]+)\\)");
     private static final Pattern ALTERNATIVE_NON_WHITESPACE_TEXT_REGEXP = Pattern.compile("([^\\s^/]+)((/[^\\s^/]+)+)");
     private static final String DOUBLE_ESCAPE = "\\\\";
@@ -19,9 +20,11 @@ public class CucumberExpression implements Expression {
     private final List<ParameterType<?>> parameterTypes = new ArrayList<>();
     private final String source;
     private final TreeRegexp treeRegexp;
+    private final ParameterTypeRegistry parameterTypeRegistry;
 
     public CucumberExpression(String expression, ParameterTypeRegistry parameterTypeRegistry) {
         this.source = expression;
+        this.parameterTypeRegistry = parameterTypeRegistry;
 
         expression = processEscapes(expression);
         expression = processOptional(expression);
@@ -29,6 +32,7 @@ public class CucumberExpression implements Expression {
         expression = processParameters(expression, parameterTypeRegistry);
         expression = "^" + expression + "$";
         treeRegexp = new TreeRegexp(expression);
+
     }
 
     private String processEscapes(String expression) {
@@ -123,7 +127,18 @@ public class CucumberExpression implements Expression {
     }
 
     @Override
-    public List<Argument<?>> match(String text) {
+    public List<Argument<?>> match(String text, Type... typeHints) {
+        List<ParameterType<?>> parameterTypes = new ArrayList<>(this.parameterTypes);
+        for (int i = 0; i < parameterTypes.size(); i++) {
+            ParameterType<?> parameterType = parameterTypes.get(i);
+            Type type = i < typeHints.length ? typeHints[i] : String.class;
+            if (parameterType.isAnonymous()) {
+                DefaultTransformer defaultTransformer = parameterTypeRegistry.getDefaultTransformer();
+                ObjectMapperTransformer transformer = new ObjectMapperTransformer(defaultTransformer, type);
+                parameterTypes.set(i, parameterType.deAnonymize(type, transformer));
+            }
+        }
+
         return Argument.build(treeRegexp, parameterTypes, text);
     }
 

cucumber-expressions/java/src/main/java/io/cucumber/cucumberexpressions/RegularExpression.java
@@ -1,13 +1,16 @@
 package io.cucumber.cucumberexpressions;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static io.cucumber.cucumberexpressions.ParameterType.createAnonymousParameterType;
+
 public class RegularExpression implements Expression {
     private final Pattern expressionRegexp;
     private final ParameterTypeRegistry parameterTypeRegistry;
-    private TreeRegexp treeRegexp;
+    private final TreeRegexp treeRegexp;
 
     /**
      * Creates a new instance. Use this when the transform types are not known in advance,
@@ -20,33 +23,36 @@ public class RegularExpression implements Expression {
     public RegularExpression(Pattern expressionRegexp, ParameterTypeRegistry parameterTypeRegistry) {
         this.expressionRegexp = expressionRegexp;
         this.parameterTypeRegistry = parameterTypeRegistry;
-        treeRegexp = new TreeRegexp(expressionRegexp);
+        this.treeRegexp = new TreeRegexp(expressionRegexp);
     }
 
     @Override
-    public List<Argument<?>> match(String text) {
-        List<ParameterType<?>> parameterTypes = new ArrayList<>();
+    public List<Argument<?>> match(String text, Type... typeHints) {
+        final DefaultTransformer defaultTransformer = parameterTypeRegistry.getDefaultTransformer();
+        final List<ParameterType<?>> parameterTypes = new ArrayList<>();
+        int typeHintIndex = 0;
         for (GroupBuilder groupBuilder : treeRegexp.getGroupBuilder().getChildren()) {
-            String parameterTypeRegexp = groupBuilder.getSource();
+            final String parameterTypeRegexp = groupBuilder.getSource();
+            final Type typeHint = typeHintIndex < typeHints.length ? typeHints[typeHintIndex++] : String.class;
 
             ParameterType<?> parameterType = parameterTypeRegistry.lookupByRegexp(parameterTypeRegexp, expressionRegexp, text);
-            if (parameterType == null) parameterType = new ParameterType<>(
-                    null,
-                    parameterTypeRegexp,
-                    String.class,
-                    new Transformer<String>() {
-                        @Override
-                        public String transform(String arg) {
-                            return arg;
-                        }
-                    }
-            );
+
+            if (parameterType == null) {
+                parameterType = createAnonymousParameterType(parameterTypeRegexp);
+            }
+
+            // Either from createAnonymousParameterType or lookupByRegexp
+            if (parameterType.isAnonymous()) {
+                Transformer<Object> transformer = new ObjectMapperTransformer(defaultTransformer, typeHint);
+                parameterType = parameterType.deAnonymize(typeHint, transformer);
+            }
+
             parameterTypes.add(parameterType);
         }
 
+
         return Argument.build(treeRegexp, parameterTypes, text);
     }
-
 
     @Override
     public Pattern getRegexp() {
@@ -57,4 +63,5 @@ public class RegularExpression implements Expression {
     public String getSource() {
         return expressionRegexp.pattern();
     }
+
 }

cucumber/cucumber-expressions

go/parameter_type_registry.go
@@ -2,9 +2,9 @@ package cucumberexpressions
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 	"sort"
-	"strconv"
 )
 
 var INTEGER_REGEXPS = []*regexp.Regexp{
@@ -20,23 +20,28 @@ var WORD_REGEXPS = []*regexp.Regexp{
 var STRING_REGEXPS = []*regexp.Regexp{
 	regexp.MustCompile(`"([^"\\]*(\\.[^"\\]*)*)"|'([^'\\]*(\\.[^'\\]*)*)'`),
 }
+var ANONYMOUS_REGEXPS = `.*`
 
 type ParameterTypeRegistry struct {
 	parameterTypeByName    map[string]*ParameterType
 	parameterTypesByRegexp map[string][]*ParameterType
+	defaultTransformer     ParameterByTypeTransformer
 }
 
 func NewParameterTypeRegistry() *ParameterTypeRegistry {
+	transformer := BuiltInParameterTransformer{}
+
 	result := &ParameterTypeRegistry{
 		parameterTypeByName:    map[string]*ParameterType{},
 		parameterTypesByRegexp: map[string][]*ParameterType{},
+		defaultTransformer:     transformer,
 	}
 	intParameterType, err := NewParameterType(
 		"int",
 		INTEGER_REGEXPS,
 		"int",
 		func(args ...*string) interface{} {
-			i, err := strconv.Atoi(*args[0])
+			i, err := transformer.Transform(*args[0], reflect.Int)
 			if err != nil {
 				panic(err)
 			}
@@ -54,7 +59,7 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		FLOAT_REGEXPS,
 		"float",
 		func(args ...*string) interface{} {
-			f, err := strconv.ParseFloat(*args[0], 64)
+			f, err := transformer.Transform(*args[0], reflect.Float64)
 			if err != nil {
 				panic(err)
 			}
@@ -72,7 +77,11 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		WORD_REGEXPS,
 		"string",
 		func(args ...*string) interface{} {
-			return *args[0]
+			i, err := transformer.Transform(*args[0], reflect.String)
+			if err != nil {
+				panic(err)
+			}
+			return i
 		},
 		false,
 		false,
@@ -87,9 +96,17 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		"string",
 		func(args ...*string) interface{} {
 			if args[0] == nil && args[1] != nil {
-				return *args[1]
+				i, err := transformer.Transform(*args[1], reflect.String)
+				if err != nil {
+					panic(err)
+				}
+				return i
 			}
-			return *args[0]
+			i, err := transformer.Transform(*args[0], reflect.String)
+			if err != nil {
+				panic(err)
+			}
+			return i
 		},
 		true,
 		false,
@@ -98,10 +115,17 @@ func NewParameterTypeRegistry() *ParameterTypeRegistry {
 		panic(err)
 	}
 	result.DefineParameterType(stringParameterType)
+
+	anonymouseParameterType, err := createAnonymousParameterType(ANONYMOUS_REGEXPS)
+	if err != nil {
+		panic(err)
+	}
+	result.DefineParameterType(anonymouseParameterType)
+
 	return result
 }
 
-func (p *ParameterTypeRegistry) ParamaterTypes() []*ParameterType {
+func (p *ParameterTypeRegistry) ParameterTypes() []*ParameterType {
 	result := make([]*ParameterType, len(p.parameterTypeByName))
 	index := 0
 	for _, parameterType := range p.parameterTypeByName {
@@ -128,12 +152,13 @@ func (p *ParameterTypeRegistry) LookupByRegexp(parameterTypeRegexp string, expre
 }
 
 func (p *ParameterTypeRegistry) DefineParameterType(parameterType *ParameterType) error {
-	if len(parameterType.Name()) > 0 {
-		if _, ok := p.parameterTypeByName[parameterType.Name()]; ok {
-			return fmt.Errorf("There is already a parameter type with name %s", parameterType.Name())
+	if _, ok := p.parameterTypeByName[parameterType.Name()]; ok {
+		if len(parameterType.Name()) == 0 {
+			return fmt.Errorf("The anonymous parameter type has already been defined")
 		}
-		p.parameterTypeByName[parameterType.Name()] = parameterType
+		return fmt.Errorf("There is already a parameter type with name %s", parameterType.Name())
 	}
+	p.parameterTypeByName[parameterType.Name()] = parameterType
 	for _, parameterTypeRegexp := range parameterType.Regexps() {
 		if _, ok := p.parameterTypesByRegexp[parameterTypeRegexp.String()]; !ok {
 			p.parameterTypesByRegexp[parameterTypeRegexp.String()] = []*ParameterType{}

java/src/main/java/io/cucumber/cucumberexpressions/CucumberExpression.java
@@ -1,5 +1,6 @@
 package io.cucumber.cucumberexpressions;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -9,7 +10,7 @@ public class CucumberExpression implements Expression {
     // Does not include (){} characters because they have special meaning
     private static final Pattern ESCAPE_PATTERN = Pattern.compile("([\\\\^\\[$.|?*+\\]])");
     @SuppressWarnings("RegExpRedundantEscape") // Android can't parse unescaped braces
-    static final Pattern PARAMETER_PATTERN = Pattern.compile("(\\\\\\\\)?\\{([^}]+)\\}");
+    static final Pattern PARAMETER_PATTERN = Pattern.compile("(\\\\\\\\)?\\{([^}]*)\\}");
     private static final Pattern OPTIONAL_PATTERN = Pattern.compile("(\\\\\\\\)?\\(([^)]+)\\)");
     private static final Pattern ALTERNATIVE_NON_WHITESPACE_TEXT_REGEXP = Pattern.compile("([^\\s^/]+)((/[^\\s^/]+)+)");
     private static final String DOUBLE_ESCAPE = "\\\\";
@@ -19,9 +20,11 @@ public class CucumberExpression implements Expression {
     private final List<ParameterType<?>> parameterTypes = new ArrayList<>();
     private final String source;
     private final TreeRegexp treeRegexp;
+    private final ParameterTypeRegistry parameterTypeRegistry;
 
     public CucumberExpression(String expression, ParameterTypeRegistry parameterTypeRegistry) {
         this.source = expression;
+        this.parameterTypeRegistry = parameterTypeRegistry;
 
         expression = processEscapes(expression);
         expression = processOptional(expression);
@@ -29,6 +32,7 @@ public class CucumberExpression implements Expression {
         expression = processParameters(expression, parameterTypeRegistry);
         expression = "^" + expression + "$";
         treeRegexp = new TreeRegexp(expression);
+
     }
 
     private String processEscapes(String expression) {
@@ -123,7 +127,18 @@ public class CucumberExpression implements Expression {
     }
 
     @Override
-    public List<Argument<?>> match(String text) {
+    public List<Argument<?>> match(String text, Type... typeHints) {
+        List<ParameterType<?>> parameterTypes = new ArrayList<>(this.parameterTypes);
+        for (int i = 0; i < parameterTypes.size(); i++) {
+            ParameterType<?> parameterType = parameterTypes.get(i);
+            Type type = i < typeHints.length ? typeHints[i] : String.class;
+            if (parameterType.isAnonymous()) {
+                ParameterByTypeTransformer defaultTransformer = parameterTypeRegistry.getDefaultParameterTransformer();
+                ObjectMapperTransformer transformer = new ObjectMapperTransformer(defaultTransformer, type);
+                parameterTypes.set(i, parameterType.deAnonymize(type, transformer));
+            }
+        }
+
         return Argument.build(treeRegexp, parameterTypes, text);
     }
 

java/src/main/java/io/cucumber/cucumberexpressions/RegularExpression.java
@@ -1,13 +1,16 @@
 package io.cucumber.cucumberexpressions;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static io.cucumber.cucumberexpressions.ParameterType.createAnonymousParameterType;
+
 public class RegularExpression implements Expression {
     private final Pattern expressionRegexp;
     private final ParameterTypeRegistry parameterTypeRegistry;
-    private TreeRegexp treeRegexp;
+    private final TreeRegexp treeRegexp;
 
     /**
      * Creates a new instance. Use this when the transform types are not known in advance,
@@ -20,33 +23,36 @@ public class RegularExpression implements Expression {
     public RegularExpression(Pattern expressionRegexp, ParameterTypeRegistry parameterTypeRegistry) {
         this.expressionRegexp = expressionRegexp;
         this.parameterTypeRegistry = parameterTypeRegistry;
-        treeRegexp = new TreeRegexp(expressionRegexp);
+        this.treeRegexp = new TreeRegexp(expressionRegexp);
     }
 
     @Override
-    public List<Argument<?>> match(String text) {
-        List<ParameterType<?>> parameterTypes = new ArrayList<>();
+    public List<Argument<?>> match(String text, Type... typeHints) {
+        final ParameterByTypeTransformer defaultTransformer = parameterTypeRegistry.getDefaultParameterTransformer();
+        final List<ParameterType<?>> parameterTypes = new ArrayList<>();
+        int typeHintIndex = 0;
         for (GroupBuilder groupBuilder : treeRegexp.getGroupBuilder().getChildren()) {
-            String parameterTypeRegexp = groupBuilder.getSource();
+            final String parameterTypeRegexp = groupBuilder.getSource();
+            final Type typeHint = typeHintIndex < typeHints.length ? typeHints[typeHintIndex++] : String.class;
 
             ParameterType<?> parameterType = parameterTypeRegistry.lookupByRegexp(parameterTypeRegexp, expressionRegexp, text);
-            if (parameterType == null) parameterType = new ParameterType<>(
-                    null,
-                    parameterTypeRegexp,
-                    String.class,
-                    new Transformer<String>() {
-                        @Override
-                        public String transform(String arg) {
-                            return arg;
-                        }
-                    }
-            );
+
+            if (parameterType == null) {
+                parameterType = createAnonymousParameterType(parameterTypeRegexp);
+            }
+
+            // Either from createAnonymousParameterType or lookupByRegexp
+            if (parameterType.isAnonymous()) {
+                Transformer<Object> transformer = new ObjectMapperTransformer(defaultTransformer, typeHint);
+                parameterType = parameterType.deAnonymize(typeHint, transformer);
+            }
+
             parameterTypes.add(parameterType);
         }
 
+
         return Argument.build(treeRegexp, parameterTypes, text);
     }
-
 
     @Override
     public Pattern getRegexp() {
@@ -57,4 +63,5 @@ public class RegularExpression implements Expression {
     public String getSource() {
         return expressionRegexp.pattern();
     }
+
 }