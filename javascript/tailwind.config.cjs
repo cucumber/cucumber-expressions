ffea370ae2a36f3c19ca2c4ea8673e4e922dc47e
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2021-09-28 22:57:01 +0100

    More matching

---
+++ javascript/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['../docs/index.html', './try/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
--- javascript/try/Try.tsx
+++ javascript/try/Try.tsx
@@ -1,39 +1,89 @@
 import React, { useMemo, useState } from 'react'
 
-import { CucumberExpression, ParameterTypeRegistry } from '../src/index'
+import {
+  Argument,
+  CucumberExpression,
+  CucumberExpressionGenerator,
+  GeneratedExpression,
+  ParameterTypeRegistry,
+} from '../src/index'
 
-type Result = {
+type ExpressionResult = {
   expression?: CucumberExpression
   error?: Error
 }
 
+type MatchResult = {
+  args?: readonly Argument[] | null
+  generatedExpressions?: readonly GeneratedExpression[]
+}
+
 export const Try: React.FunctionComponent = () => {
   const reg = useMemo(() => new ParameterTypeRegistry(), [])
-  const [src, setSrc] = useState('I have {int} cukes in my {word}')
+  const gen = useMemo(() => new CucumberExpressionGenerator(() => reg.parameterTypes), [reg])
+  const [src, setSrc] = useState('I have {int} cukes in my {word')
   const [text, setText] = useState('I have 42 cukes in my belly')
-  const result = useMemo<Result>(() => {
+  const result = useMemo<ExpressionResult>(() => {
     try {
       return { expression: new CucumberExpression(src, reg) }
     } catch (error) {
       return { error }
     }
   }, [src])
-  const args = useMemo(() => result.expression?.match(text), [result, text])
+  const matchResult = useMemo<MatchResult>(() => {
+    const args = result.expression?.match(text)
+    if (args) {
+      return { args }
+    } else {
+      return { generatedExpressions: gen.generateExpressions(text) }
+    }
+  }, [result, text])
 
   return (
-    <div>
-      <h1>Try Cucumber Expressions yo</h1>
-      <input type="text" value={src} onChange={(e) => setSrc(e.target.value)} />
-      <pre>/{result.expression?.regexp.source || result.error?.message}/</pre>
-      <br />
-      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-      {(args && (
-        <ol>
-          {args.map((arg, i) => (
-            <li key={i}>{arg.getValue(null)}</li>
-          ))}
-        </ol>
-      )) || <div>No match</div>}
+    <div className="grid grid-cols-1 gap-4">
+      <label className="block">
+        <span className="text-gray-700">Cucumber Expression</span>
+        <input
+          type="text"
+          className="mt-1 block w-full"
+          value={src}
+          onChange={(e) => setSrc(e.target.value)}
+        />
+      </label>
+
+      {result.expression && <pre>/{result.expression.regexp.source}/</pre>}
+      {result.error && <pre className="p-2 border-4 border-red-500">{result.error.message}</pre>}
+
+      <label className="block">
+        <span className="text-gray-700">Step text</span>
+        <input
+          type="text"
+          className="mt-1 block w-full"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </label>
+
+      {matchResult.args && (
+        <div>
+          <span className="text-gray-700">Arguments</span>
+          <ol className="list-decimal list-inside">
+            {matchResult.args.map((arg, i) => (
+              <li key={i}>{arg.getValue(null)}</li>
+            ))}
+          </ol>
+        </div>
+      )}
+      {matchResult.generatedExpressions && (
+        <div>
+          <span className="text-gray-700">Arguments</span>
+          <ul className="list-disc list-inside">
+            {matchResult.generatedExpressions.map((generatedExpression, i) => (
+              <li key={i}>{generatedExpression.source}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
