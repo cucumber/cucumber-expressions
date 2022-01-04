cde50139d1a51c756f98b9a0ec16c2420fbd2516
Aslak Hellesøy <1000+aslakhellesoy@users.noreply.github.com>
2022-01-04 13:07:44 +0000

    Update java/src/test/java/io/cucumber/cucumberexpressions/CucumberExpressionTest.java
    Co-authored-by: Aurélien Reeves <aurelien.reeves@smartbear.com>

--- java/src/test/java/io/cucumber/cucumberexpressions/CucumberExpressionTest.java
+++ java/src/test/java/io/cucumber/cucumberexpressions/CucumberExpressionTest.java
@@ -40,7 +40,6 @@ class CucumberExpressionTest {
         List<Path> paths = new ArrayList<>();
         newDirectoryStream(Paths.get("..", "testdata", "cucumber-expression", "matching")).forEach(paths::add);
         paths.sort(Comparator.naturalOrder());
-//        return paths;
         return singletonList(Paths.get("..", "testdata", "cucumber-expression", "matching", "matches-byte.yaml"));
     }
 
