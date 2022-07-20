e779677db2f4dc0b7ed4e4d64e05acd441ff7681
aurelien-reeves <aurelien.reeves@smartbear.com>
2022-07-20 11:44:36 +0200

    Use 'glob' to import all tests automatically

--- javascript/test/minispec_entrypoint.ts
+++ javascript/test/minispec_entrypoint.ts
@@ -1,17 +1,15 @@
-import './ArgumentTest.js'
-import './CombinatorialGeneratedExpressionFactoryTest.js'
-import './CucumberExpressionGeneratorTest.js'
-import './CucumberExpressionParserTest.js'
-import './CucumberExpressionTest.js'
-import './CucumberExpressionTokenizerTest.js'
-import './CucumberExpressionTransformationTest.js'
-import './CustomParameterTypeTest.js'
-import './ExpressionFactoryTest.js'
-import './ParameterTypeRegistryTest.js'
-import './ParameterTypeTest.js'
-import './RegularExpressionTest.js'
-import './TreeRegexpTest.js'
-
+import glob from 'glob'
 import MiniSpec from 'minispec'
 
-MiniSpec.execute()
+async function importTests () {
+  for (const testFile of glob.sync('./test/*Test.ts')) {
+    console.log(testFile)
+    await import(testFile.replace('./test/', './').replace('.ts', '.js'))
+  }
+}
+
+importTests().then(() => {
+  MiniSpec.execute()
+})
+
+
