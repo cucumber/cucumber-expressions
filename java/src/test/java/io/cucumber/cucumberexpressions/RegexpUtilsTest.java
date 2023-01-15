b2febb71364d7b02d57e85ba5bd84b4b74e1389f
M.P. Korstanje <rien.korstanje@gmail.com>
2023-01-15 13:02:10 +0100

    Add coverage for flush

--- java/src/test/java/io/cucumber/cucumberexpressions/RegexpUtilsTest.java
+++ java/src/test/java/io/cucumber/cucumberexpressions/RegexpUtilsTest.java
@@ -2,29 +2,35 @@ package io.cucumber.cucumberexpressions;
 
 import org.junit.jupiter.api.Test;
 
+import static io.cucumber.cucumberexpressions.RegexpUtils.escapeRegex;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RegexpUtilsTest {
 
     @Test
+    void escape_regex_characters(){
+        assertEquals("hello \\$world", escapeRegex("hello $world"));
+    }
+
+    @Test
     void escape_all_regexp_characters() {
-        assertEquals("\\^\\$\\[\\]\\(\\)\\{\\}\\.\\|\\?\\*\\+\\\\", RegexpUtils.escapeRegex("^$[](){}.|?*+\\"));
+        assertEquals("\\^\\$\\[\\]\\(\\)\\{\\}\\.\\|\\?\\*\\+\\\\", escapeRegex("^$[](){}.|?*+\\"));
     }
 
     @Test
     void escape_escaped_regexp_characters() {
-        assertEquals("\\^\\$\\[\\]\\\\\\(\\\\\\)\\{\\}\\\\\\\\\\.\\|\\?\\*\\+", RegexpUtils.escapeRegex("^$[]\\(\\){}\\\\.|?*+"));
+        assertEquals("\\^\\$\\[\\]\\\\\\(\\\\\\)\\{\\}\\\\\\\\\\.\\|\\?\\*\\+", escapeRegex("^$[]\\(\\){}\\\\.|?*+"));
     }
 
 
     @Test
     void do_not_escape_when_there_is_nothing_to_escape() {
-        assertEquals("dummy", RegexpUtils.escapeRegex("dummy"));
+        assertEquals("hello world", escapeRegex("hello world"));
     }
 
     @Test
-    void escapeRegex_gives_no_error_for_unicode_characters() {
-        assertEquals("🥒", RegexpUtils.escapeRegex("🥒"));
+    void gives_no_error_for_unicode_characters() {
+        assertEquals("🥒", escapeRegex("🥒"));
     }
 
 }
