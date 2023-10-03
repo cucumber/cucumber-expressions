ba5167bf5735ffdc3c2b429e6027035bb252862e
Luke Hill <lukehill_uk@hotmail.com>
2023-10-03 18:05:02 +0100

    AF: Style/LineEndConcatenation

--- ruby/spec/cucumber/cucumber_expressions/parameter_type_registry_spec.rb
+++ ruby/spec/cucumber/cucumber_expressions/parameter_type_registry_spec.rb
@@ -53,27 +53,27 @@ module Cucumber
           expect(@registry.lookup_by_regexp(CAPITALISED_WORD.source, /([A-Z]+\w+) and ([A-Z]+\w+)/, "Lisa and Bob")).to eq(person)
         end.to raise_error(
           CucumberExpressionError,
-          "Your Regular Expression /([A-Z]+\\w+) and ([A-Z]+\\w+)/\n" +
-            "matches multiple parameter types with regexp /[A-Z]+\\w+/:\n" +
-            "   {name}\n" +
-            "   {person}\n" +
-            "   {place}\n" +
-            "\n" +
-            "I couldn't decide which one to use. You have two options:\n" +
-            "\n" +
-            "1) Use a Cucumber Expression instead of a Regular Expression. Try one of these:\n" +
-            "   {name} and {name}\n" +
-            "   {name} and {person}\n" +
-            "   {name} and {place}\n" +
-            "   {person} and {name}\n" +
-            "   {person} and {person}\n" +
-            "   {person} and {place}\n" +
-            "   {place} and {name}\n" +
-            "   {place} and {person}\n" +
-            "   {place} and {place}\n" +
-            "\n" +
-            "2) Make one of the parameter types preferential and continue to use a Regular Expression.\n" +
-            "\n"
+          "Your Regular Expression /([A-Z]+\\w+) and ([A-Z]+\\w+)/\n" \
+          "matches multiple parameter types with regexp /[A-Z]+\\w+/:\n" \
+          "   {name}\n" \
+          "   {person}\n" \
+          "   {place}\n" \
+          "\n" \
+          "I couldn't decide which one to use. You have two options:\n" \
+          "\n" \
+          "1) Use a Cucumber Expression instead of a Regular Expression. Try one of these:\n" \
+          "   {name} and {name}\n" \
+          "   {name} and {person}\n" \
+          "   {name} and {place}\n" \
+          "   {person} and {name}\n" \
+          "   {person} and {person}\n" \
+          "   {person} and {place}\n" \
+          "   {place} and {name}\n" \
+          "   {place} and {person}\n" \
+          "   {place} and {place}\n" \
+          "\n" \
+          "2) Make one of the parameter types preferential and continue to use a Regular Expression.\n" \
+          "\n"
         )
       end
     end
