ed53c3166bca7d91036f939eb11d85815cca2e2f
M.P. Korstanje <rien.korstanje@gmail.com>
2026-01-15 21:22:49 +0100

    Reapply "Introduce module"
    This reverts commit 84c71cd5aeb8f4a444197a1dab56f41a3895d20f.

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module io.cucumber.messages {
     requires org.jspecify;
-    
+    requires org.apiguardian.api;
+
     exports io.cucumber.cucumberexpressions;
+    
+    uses io.cucumber.cucumberexpressions.PatternCompiler;
 }
