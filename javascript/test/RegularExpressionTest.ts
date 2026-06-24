44018bbf9b5a90e0764c56879bc427694955d451
M.P. Korstanje <rien.korstanje@gmail.com>
2026-06-24 16:35:15 +0200

    Fix imports

--- javascript/test/RegularExpressionTest.ts
+++ javascript/test/RegularExpressionTest.ts
@@ -1,7 +1,7 @@
 import assert from 'node:assert'
 import fs from 'node:fs'
 import { glob } from 'glob'
-import yaml from 'js-yaml'
+import * as yaml from 'js-yaml'
 
 import ParameterTypeRegistry from '../src/ParameterTypeRegistry.js'
 import RegularExpression from '../src/RegularExpression.js'
